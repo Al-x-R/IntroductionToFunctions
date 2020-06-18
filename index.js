6811c60d94502fcdaab7c4a2438f5be8131de295
Alexander Roshchin <alexander.roshchin85@gmail.com>
2020-06-18 22:56:50 +0300

    Add checkMultiplicity function

--- index.js
+++ index.js
@@ -6,3 +6,13 @@
 function isAdult(age) {
     return age >= 18
 }
+
+/**
+ * Checks the multiplicity of the first number to the second
+ * @param {Number} m
+ * @param {Number} n
+ * @returns {boolean}
+ */
+function checkMultiplicity(m, n) {
+    return m % n === 0
+}
